8f77bf96845c8f857e235aa43033c856bfd9ab88
Tomasz Paterka <tomekpaterka@gmail.com>
2018-05-12 15:16:16 +0200

    add scripts

--- script.js
+++ script.js
@@ -5,14 +5,67 @@ function makeGreen() {
 	p.style.fontSize = '50px';
 }
 // Regular
+console.log('hello');
+
 // Interpolated
+console.log('Hello i am a %s string!', 'poo');
+
 // Styled
+//console.log('%c I am some great text', 'font-size: 50px; background: blue; text-shadow: 10px 10px 0 red');
+
 // warning!
+console.warn('OH NOOOO');
+
 // Error :|
+console.error('Shit!');
+
 // Info
+console.info('Crocodiles eat 3-4 people per year');
+
 // Testing
+const p = document.querySelector('p');
+console.assert(p.classList.contains('ouch'), 'That is wrong!'); // if its true nothing will happend, if its false its show the statement
+
 // clearing
+console.clear();
+
 // Viewing DOM Elements
+console.log(p);
+console.dir(p);
+
+console.clear();
 // Grouping together
+dogs.forEach(dog => {
+	console.group(`${dog.name}`);
+	console.log(`This is ${dog.name}`);
+	console.log(`${dog.name} is ${dog.age} years old`);
+	console.log(`${dog.name} is ${dog.age *7} dog years old`);
+	console.groupEnd(`${dog.name}`);
+});
+
 // counting
+console.count('Tom');
+console.count('Tom');
+console.count('Steve');
+console.count('Tom');
+console.count('Tom');
+console.count('Steve');
+console.count('Tom');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+
 // timing
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
+	.then(data => data.json())
+	.then(data => {
+		console.timeEnd('fetching data');
+		console.log(data);
+	});
+	
+console.table(dogs);
+	
+
+
